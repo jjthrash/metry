8b95875d56d78256187349394d35cd18811e5027
Nathaniel Talbott <nathaniel@terralien.com>
2009-07-06 16:08:03 -0400

    Added tracking of referrer and user agent. Added expiration and path to cookie.

--- lib/metry/rack/tracking.rb
+++ lib/metry/rack/tracking.rb
@@ -11,7 +11,7 @@ module Metry
       def call(env)
         request = ::Rack::Request.new(env)
         visitor = find_visitor(request)
-        env["metry.event"] = event = build_event(visitor, request)
+        env["metry.event"] = event = build_event(visitor, request, env)
 
         status, headers, body = @app.call(env)
 
@@ -24,14 +24,16 @@ module Metry
         response.to_a
       end
     
-      def build_event(visitor, request)
+      def build_event(visitor, request, env)
         { "event" => "pageview",
           "path" => request.fullpath,
           "time" => Time.now.to_f.to_s.sub(/\.(\d+)/){|m| [$1[0...5].ljust(5, "0")]},
           "visitor" => visitor,
           "ip" => request.ip,
           "host" => request.host,
-          "method" => request.request_method }
+          "method" => request.request_method,
+          "referrer" => env["HTTP_REFERER"],
+          "user_agent" => env["HTTP_USER_AGENT"] }
       end
     
       def find_visitor(request)
@@ -39,7 +41,10 @@ module Metry
       end
     
       def save_visitor(response, visitor)
-        response.set_cookie(COOKIE, visitor)
+        response.set_cookie(COOKIE,
+          :value => visitor,
+          :expires => (Time.now+(60*60*24*365*20)),
+          :path => '/')
       end
     end
   end
